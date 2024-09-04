f2004546700baa869813545c9820adb88eccef0b
Bulmaro Noguera <80559551+alexomanov@users.noreply.github.com>
2024-09-04 16:57:50 -0500

    Add files via upload

---
+++ 01_hello_world.rb
@@ -0,0 +1,10 @@
+=begin
+**********************************************************
+* Program:              01_hello_world.rb
+* Purpose:              print in screen the phrase Hello World
+* Version:              1.0    
+* Author:               Eng. Bulmaro Noguera
+**********************************************************
+=end
+
+puts "Hello World!!!"
